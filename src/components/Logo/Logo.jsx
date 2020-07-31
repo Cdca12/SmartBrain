fac2d2cf8daff38080cf5fc5757d9c074f69e131
Carlos Contreras <cdca12mx@gmail.com>
2020-07-31 15:29:16 -0600

    Brain logo resize

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,9 +6,9 @@ import './Logo.css'
 const Logo = () => {
     return (
         <div className="ma4 mt0 ">
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 200, width: 200 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 120, width: 120 }} >
                 <div className="Tilt-inner pa3">
-                    <img src={brainLogo} alt="Logo" style={{ height: '150px', width: '150px', padding: '10px' }}/>
+                    <img src={brainLogo} alt="Logo" style={{ height: '80px', width: '80px', padding: '5px' }}/>
                 </div>
             </Tilt>
         </div>
