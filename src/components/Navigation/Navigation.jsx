6c0dbeb5ad9aed32aef9e5cf934ef2c2cffdb8e3
Carlos Contreras <cdca12mx@gmail.com>
2020-07-30 23:27:06 -0600

    Created Navigation component and added packages

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => {
+    return(
+        <div className="navigation">
+            <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+        </div>
+    );
+}
+
+export default Navigation;
