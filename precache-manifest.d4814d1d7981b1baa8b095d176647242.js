3c147e6456b8855c980e51e4dd7a8cfe959d4e17
Carlos Contreras <cdca12mx@gmail.com>
2020-08-01 12:27:27 -0600

    Publish v0.1.0

---
+++ precache-manifest.d4814d1d7981b1baa8b095d176647242.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6de58052f0ed57dedcb8051eb87c65",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "db85dae9690cf9422aa8",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "563008f3dacf66da8793",
+    "url": "/SmartBrain/static/css/main.56dc0591.chunk.css"
+  },
+  {
+    "revision": "db85dae9690cf9422aa8",
+    "url": "/SmartBrain/static/js/2.40c63c8a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrain/static/js/2.40c63c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563008f3dacf66da8793",
+    "url": "/SmartBrain/static/js/main.2ac60eae.chunk.js"
+  },
+  {
+    "revision": "405698958a3472004a72",
+    "url": "/SmartBrain/static/js/runtime-main.db396b0b.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/SmartBrain/static/media/brain.4100098c.png"
+  }
+]);
