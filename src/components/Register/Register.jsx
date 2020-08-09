64b7d71f366c64278d7f6891a9af74400b9649b7
Carlos Contreras <cdca12mx@gmail.com>
2020-08-09 16:09:26 -0600

    Changed email to username

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -16,8 +16,8 @@ const Register = (props) => {
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
                         </div>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+                            <label className="db fw6 lh-copy f6" htmlFor="username">Username</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="username" id="username" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
