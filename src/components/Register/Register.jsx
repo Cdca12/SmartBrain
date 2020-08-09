febdced08d7766b1b20b4516a69ddd5e900310fc
Carlos Contreras <cdca12mx@gmail.com>
2020-08-08 22:56:41 -0600

    Back to first sign up
    And changed button color to blue

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Register = (props) => {
 
-    // const { onRouteChange } = props;
+    const { onRouteChange } = props;
 
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -26,7 +26,7 @@ const Register = (props) => {
                     </fieldset>
                     <div className="">
                         <input
-                            // onClick={() => onRouteChange("home")}
+                            onClick={() => onRouteChange("home")}
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                             type="submit"
                             value="Register"
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SignIn = (props) => {
 
-    const { handleUsernameChange, handlePasswordChange, onRouteChange } = props;
+    const { onRouteChange } = props;
 
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -12,12 +12,12 @@ const SignIn = (props) => {
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Username</label>
-                            <input onChange={handleUsernameChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input onChange={handlePasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
                         </div>
                     </fieldset>
                     <div className="">
